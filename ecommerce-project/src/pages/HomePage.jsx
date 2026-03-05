a35b727ce3390b9df51a0029c1046944d4c766e6
zaku1122 <zakwan.zahid@chainfore.com>
2026-03-05 16:12:13 +0500

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import { products } '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+    .then((response) => {
+        return response.json();
+    }).then((data ) => {
+        console.log(data);
+    });
 
     return (
 
